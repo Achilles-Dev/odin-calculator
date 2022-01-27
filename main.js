9f4e46bed3f69b51bb5f3299a60034230637d8f0
Solomon Achilles <solohag92@gmail.com>
2022-01-27 15:51:41 +0000

    get 2 number operation working

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-const numbers = document.querySelectorAll('.operand');
+const buttons = document.querySelectorAll('button');
 const display = document.querySelector('.display');
-const operators = document.querySelectorAll('.operator');
-let firstNumber = 0;
-let secondNumber = 0;
-let sign = '';
-
+let displayValue = '0';
+let firstNumber = null;
+let secondNumber = null;
+let firstOperator = null;
+let secondOperator = null;
+let results = 0;
 
 function add(a, b) {
     return a + b;
@@ -25,16 +26,16 @@ function divide(a, b) {
 function operate(operator, a, b) {
     let results = 0;
     switch(operator){
-        case '+': 
+        case '107': 
             results =  add(a, b);
             break;
-        case '-':
+        case '109':
             results = subtract(a, b);
             break;
-        case 'x':
+        case '106':
             results = multiply(a, b);
             break; 
-        case '/':
+        case '111':
             results = divide(a, b);
             break;
         default:
@@ -43,6 +44,82 @@ function operate(operator, a, b) {
     return results;
 }
 
+function updateDisplay(){
+    display.textContent = displayValue;
+    if (displayValue.length > 9){
+        display.textContent = displayValue.substring(0,9)
+    }
+}
+
+
+buttons.forEach(button => button.addEventListener('click', () => {
+    
+    if (button.className === 'operand'){
+        inputOperand(button.textContent);
+        updateDisplay();
+    } else if (button.className === 'operator'){
+        inputOperator(button['dataset'].set);
+        updateDisplay();
+    } else if (button.className === 'equal'){
+        inputEqualSign();
+        updateDisplay();
+        displayValue = null;
+    }
+}))
+
+function inputOperand(number) {
+    if (firstNumber === null){
+        if (displayValue === '0' || displayValue === 0){
+            displayValue = number;
+        }else if (displayValue === firstNumber){
+            //new operation after inputEqualSign()
+            displayValue = number;
+        }else {
+            displayValue += number;
+        }
+    } else if (secondOperator != null) {
+        secondNumber = number;
+    }
+    else {
+        if (displayValue === firstNumber){
+            displayValue = number;
+        } else {
+            displayValue += number;
+        }
+    }
+
+    console.log(firstNumber);
+}
+
+
+function inputOperator(operator) {   
+
+    if (firstOperator != null){
+        firstOperator = operator;
+        secondNumber = displayValue;
+    } else {
+        firstOperator = operator;
+        firstNumber = displayValue;
+    }
+      
+
+};
+
+
+function inputEqualSign(){
+    if (firstNumber === null){
+        displayValue = displayValue;
+    } else {
+        secondNumber = displayValue;
+        results = operate(firstOperator, Number(firstNumber), Number(secondNumber)).toString();
+        displayValue = results;
+        firstNumber = null;
+        secondNumber = null;
+        firstOperator = null;
+        secondOperator = null;
+    }
+   
+};
 
 
 
